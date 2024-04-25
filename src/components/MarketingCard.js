bab3f6d059992a2e22b0f6eeb2f205c25d7f5eca
ayak1 <ayakilliah2163@gmail.com>
2024-04-25 12:01:12 +0300

    set icon and title

--- src/components/MarketingCard.js
+++ src/components/MarketingCard.js
@@ -5,8 +5,9 @@ export const MarketingCard = ({text,bg_color,text_color}) => {
         background:bg_color,
         color:text_color,
     }
+    
     return(
-        <Col className="marketing_card ak_shadow  d-flex justify-content-center align-items-center" style={style}>
+        <Col className={`marketing_card ak_shadow  d-flex justify-content-center align-items-center`} style={style}>
             <p className="marketing-text tajawal-bold">{text}</p>
         </Col>
     )
